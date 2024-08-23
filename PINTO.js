52d0efb2b561f1c41229261ad0621f58bd504407
Brasolio <miguel.nossabein@escola.pr.gov.br>
2024-08-23 12:29:33 -0300

    hg

--- PINTO.js
+++ PINTO.js
@@ -1,8 +1,8 @@
 const caixaPrincipal = document.querySelector ('caixa-Principal');
-const caixaPrincipal = document.querySelector ('caixa-Principal');
-const caixaPrincipal = document.querySelector ('caixa-Principal');
-const caixaPrincipal = document.querySelector ('caixa-Principal');
-const caixaPrincipal = document.querySelector ('caixa-Principal');
+const caixaPerguntas = document.querySelector ('caixa-Perguntas');
+const caixaAlternativas = document.querySelector ('caixa-Alternativa');
+const caixaResultado = document.querySelector ('caixa-Resultado');
+const textoresultado = document.querySelector ('texto-resultado');
 
 const perguntas = [
     {
@@ -49,9 +49,36 @@ const perguntas = [
 ]
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta (){
+    if (atual>= perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual[atual];
     caixaPerguntas.textContext = perguntaAtual.enunciado;
     caixaAlternativas.textContext = ""
+}
+
+function mostraAlternativa(){
+    for(const pergunta of perguntaAtual.alternativas){
+        const botaoAlternativa = document.createElement('button');
+        botaoAlternativa.textContent = pergunta.texto;
+        botaoAlternativa.addEventListener ("click",()=> respostaSelecionada(pergunta));
+    }
+
+}
+
+function respostaSelecionada (pergunta){
+    const afirmacoes = pergunta.afirmação;
+historiaFinal += afirmacoes + " ";
+atual++;
+mostraPergunta();
+}
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "Em resumo, voce escolheu...";
+        caixaResultado.textContent = historiaFinal;
+        caixaAlternativas.textContent ="";
 }
