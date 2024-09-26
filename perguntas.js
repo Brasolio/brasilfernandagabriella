d846d36fca2af5b0486f2e485b961e98e3b882a2
Brasolio <miguel.nossabein@escola.pr.gov.br>
2024-09-26 08:37:42 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,80 @@
+criaCartao(
+  `Matemática´
+  `Qual número vem depois do nove?´
+  `Dez´
+)
+
+criarCartao(
+  `Biologia´
+  `Quantos pares de cromosomos tem um humano?´
+  `23 pares´
+)
+
+  criarCartao(
+  `Projeto de vida´
+  `Voce tem currículo?´
+  `Nao´
+)
+
+  criarCartao(
+  `Geografia´
+  `Quais sao os 3 planaltos do estado do Paraná?´
+  `Curitiba, Ponta Grossa e Guarapuava´
+)
+
+  criarCartao(
+  `Filosofia´
+  `Qual filosofo disse a frase ´´ só sei que nada sei ``?´
+  `Sócrates´
+)
+
+  criarCartao(
+  `Química´
+  `Qual é a fórmula da água?´
+  `H2O´
+)
+
+  criarCartao(
+  `Física´
+  `Qual é o nome da medida de temperatura nos Estados Unidos?´
+  `Graus Fahrenheit´
+)
+
+  criarCartao(
+  `Portugues´
+  `Qual é a época do romantismo?´
+  `Século 18 e 19´
+)
+
+  criarCartao(
+  `Astronomia´
+  `Qual é o nome do maior planeta do sistema solar?´
+  `Júpiter´
+)
+
+  criarCartao(
+  `Biologia´
+  `Qual é o animal com 6 sentidos?´
+  `Ornitirrinco´
+)
+
+  criarCartao(
+  `Hitória´
+  `Qual é a data da queda da Bastilha?´
+  `14/07/1789´
+)     
+
+  criarCartao(
+  `Física´
+  `Qual é o nome do satélite natural da Terra?´
+  `Lua´
+)
+
+  criarCartao(
+  `Robótica´
+  `Qual é o cabo que usamos para conectar o arduíno no computador?´
+  `Cabo USB´
+)
+
+  criar Cartao(
+  
